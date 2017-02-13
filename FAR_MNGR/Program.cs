b1265b9d0605ee69aed7782db376e3ac5e24c7f6
Zharaskhan Aman <aman.zharaskhan@gmail.com>
2017-02-13 00:34:26 -0800

    Add project files.

---
+++ FAR_MNGR/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAR_MNGR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
